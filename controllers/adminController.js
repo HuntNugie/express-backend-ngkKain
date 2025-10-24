37dcafa44d1a5607d2815f046943b85688ee1d33
Nugie kurniawan <nugiebinhar@gmail.com>
2025-10-24 21:44:44 +0700

    reafactoring/membuat halaman admin di pisahkan menjadi controller terpisah

---
+++ controllers/adminController.js
@@ -0,0 +1,18 @@
+export const dashboardController = (req,res)=>{
+      res.status(200).json({nama:req.admin.nama,email:req.admin.email,role:req.admin.role})
+}
+export const addContactForm = (req,res)=>{
+    res.status(200).json({nama:req.admin.nama,email:req.admin.email,role:req.admin.role})
+}
+export const addContactStore = async(req,res)=>{
+    try {
+    const {nama_produk,kategori,harga,stok,deskripsi} = req.body
+    const result = await Produk.create(req.body)
+    res.status(200).json({nama:req.admin.nama,email:req.admin.email,role:req.admin.role,data:result})
+    } catch (error) {
+    throw error    
+    }
+}
+export const produkController = (req,res)=>{
+    res.status(200).json({nama:req.admin.nama,email:req.admin.email,role:req.admin.role})
+}
--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import { auth } from "../middlewares/authMiddleware.js";
+import Produk from "../models/Produk/produk.model.js";
+import { addContactForm, addContactStore, dashboardController, produkController } from "../controllers/adminController.js";
 
 const route = Router();
 
@@ -8,18 +10,8 @@ route.use(auth);
 route.get("/check",(req,res)=>{
       res.status(200).json({nama:req.admin.nama,email:req.admin.email,role:req.admin.role})
 })
-route.get("/produk",(req,res)=>{
-        res.status(200).json({nama:req.admin.nama,email:req.admin.email,role:req.admin.role})
-})
-route.get("/add-produk",(req,res)=>{
-    res.status(200).json({nama:req.admin.nama,email:req.admin.email,role:req.admin.role})
-})
-route.post("/produk/add",(req,res)=>{
-    const {nama_produk,kategori,harga,stok,deskripsi} = req.body
-    
-    res.status(200).json({nama:req.admin.nama,email:req.admin.email,role:req.admin.role})
-})
-route.get("/dashboard",(req,res)=>{
-    res.status(200).json({nama:req.admin.nama,email:req.admin.email,role:req.admin.role})
-})
+route.get("/produk",produkController)
+route.get("/add-produk",addContactForm)
+route.post("/produk/add",addContactStore)
+route.get("/dashboard",dashboardController)
 export default route
