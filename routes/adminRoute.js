1cc129731d75142b784a08e74a566b75cd79fa03
Nugie kurniawan <nugiebinhar@gmail.com>
2025-10-24 20:05:52 +0700

    endpoint/membuat route untuk nampilkan form produk dan buat juga untuk route add produk walaupun belum di aktifkan fungsi nya

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -11,6 +11,14 @@ route.get("/check",(req,res)=>{
 route.get("/produk",(req,res)=>{
         res.status(200).json({nama:req.admin.nama,email:req.admin.email,role:req.admin.role})
 })
+route.get("/add-produk",(req,res)=>{
+    res.status(200).json({nama:req.admin.nama,email:req.admin.email,role:req.admin.role})
+})
+route.post("/produk/add",(req,res)=>{
+    const {nama_produk,kategori,harga,stok,deskripsi} = req.body
+    
+    res.status(200).json({nama:req.admin.nama,email:req.admin.email,role:req.admin.role})
+})
 route.get("/dashboard",(req,res)=>{
     res.status(200).json({nama:req.admin.nama,email:req.admin.email,role:req.admin.role})
 })
