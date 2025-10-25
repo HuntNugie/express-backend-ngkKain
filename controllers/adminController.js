99bcae142b90e5da5a8f312b13f0d35dd415c837
Nugie kurniawan <nugiebinhar@gmail.com>
2025-10-25 20:22:40 +0700

    fitur/membuat fitur endpoint untuk menhapus data

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,10 +2,10 @@ import Produk from "../models/Produk/produk.model.js"
 export const dashboardController = (req,res)=>{
       res.status(200).json({nama:req.admin.nama,email:req.admin.email,role:req.admin.role})
 }
-export const addContactForm = (req,res)=>{
+export const addProdukForm = (req,res)=>{
     res.status(200).json({nama:req.admin.nama,email:req.admin.email,role:req.admin.role})
 }
-export const addContactStore = async(req,res)=>{
+export const addProdukStore = async(req,res)=>{
     try {
     const result = await Produk.create(req.body)
     res.status(200).json({nama:req.admin.nama,email:req.admin.email,role:req.admin.role,data:result})
@@ -16,4 +16,19 @@ export const addContactStore = async(req,res)=>{
 export const produkController = async(req,res)=>{
     const data = await Produk.find()
     res.status(200).json({nama:req.admin.nama,email:req.admin.email,role:req.admin.role,data})
+}
+
+export const deleteProduk = async(req,res)=>{
+    try {
+        const id = req.params.id
+        console.log(id)
+        const deletes = await Produk.findByIdAndDelete(id)
+        if(deletes){
+            return res.status(200).json({message:"Produk berhasil di hapus"})
+        }else{
+            return res.status(404).json({message:"data produk tidak bisa di temukan"})
+        }
+    } catch (error) {
+        return res.status(400).json({message:`gagal menghapus karna ${error}`})
+    }
 }
--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { auth } from "../middlewares/authMiddleware.js";
-import { addContactForm, addContactStore, dashboardController, produkController } from "../controllers/adminController.js";
+import {  addProdukForm, addProdukStore, dashboardController, deleteProduk, produkController } from "../controllers/adminController.js";
 import { validatorAddProduk } from "../middlewares/validatorAddProduk.js";
 import { handlerValidator } from "../middlewares/handlerValidator.js";
+import { checkUser } from "../middlewares/checkUser.js";
 
 const route = Router();
 
@@ -12,7 +13,8 @@ route.get("/check",(req,res)=>{
       res.status(200).json({nama:req.admin.nama,email:req.admin.email,role:req.admin.role})
 })
 route.get("/produk",produkController)
-route.get("/add-produk",addContactForm)
-route.post("/produk/add",validatorAddProduk,handlerValidator,addContactStore)
+route.get("/add-produk",addProdukForm)
+route.post("/produk/add",validatorAddProduk,handlerValidator,addProdukStore)
+route.delete("/produk/delete/:id",deleteProduk)
 route.get("/dashboard",dashboardController)
 export default route
